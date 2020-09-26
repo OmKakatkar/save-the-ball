6e2e07b12cb5d71db3dc0b02b4fb576b293b4aa4
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2020-09-26 17:06:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import turtle
 import time
+import sys
 
 # Set the default window properties
 window = turtle.Screen()
 window.bgcolor("Black")
 window.setup(500, 500)
-window.title("BREAKOUT")
+window.title("SAVE THE BALL")
 window.tracer(0)
+
+multiplier = 100
+score = 0
 
 # Create a board
 board = turtle.Turtle()
@@ -24,17 +28,19 @@ ball.color("white")
 ball.shape("circle")
 ball.pu()
 # delta change for ball
-dx = 0.1
-dy = -0.08
+dx = 1
+dy = -1
 
-# Create a brick
-b = turtle.Turtle()
-b.speed(0)
-b.shape("square")
-b.color("white")
-b.shapesize(stretch_wid=2, stretch_len=3)  # Width = 20, Length = 30
-b.pu()
-b.goto(-130, 220)
+# Create a pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.ht()
+pen.pu()
+pen.goto((100, 200))
+pen.pd()
+pen.write(f"Score : {score}", font=("Arial", 16, "normal"))
+pen.pu()
 
 
 def board_left():
@@ -43,7 +49,7 @@ def board_left():
     """
     x = board.xcor()
     if x >= -210:
-        x -= 10
+        x -= 20
         board.setx(x)
 
 
@@ -53,7 +59,7 @@ def board_right():
     """
     x = board.xcor()
     if x <= 210:
-        x += 10
+        x += 20
         board.setx(x)
 
 
@@ -63,41 +69,57 @@ window.onkeypress(board_left, "Left")
 window.onkeypress(board_right, "Right")
 
 last_time = time.perf_counter()
-while True:
+try:
+    while True:
 
-    current_time = time.perf_counter()
-    elapsed_time = current_time - last_time
-    last_time = current_time
+        current_time = time.perf_counter()
+        elapsed_time = current_time - last_time
+        last_time = current_time
 
-    window.update()
+        window.update()
 
-    ball.sety(ball.ycor() + dy)
-    ball.setx(ball.xcor() + dx)
+        ball.sety(ball.ycor() + dy*elapsed_time*multiplier)
+        ball.setx(ball.xcor() + dx*elapsed_time*multiplier)
 
-    if ball.ycor() > 240:
-        ball.sety(240)
-        dy *= -1
+        if ball.ycor() > 240:
+            ball.sety(240)
+            dy *= -1
 
-    elif ball.ycor() < -240:
-        ball.goto(0, 0)
-        dy *= 1
+        elif ball.ycor() < -240:
+            ball.goto(0, 0)
+            score = 0
+            multiplier = 100
+            pen.goto((100, 200))
+            pen.clear()
+            pen.pd()
+            pen.write(f"Score : {score}", font=("Arial", 16, "normal"))
+            pen.pu()
 
-    if ball.xcor() > 240:
-        ball.setx(240)
-        dx *= -1
+            dy *= 1
 
-    elif ball.xcor() < -240:
-        ball.setx(-240)
-        dx *= -1
+        if ball.xcor() > 240:
+            ball.setx(240)
+            dx *= -1
 
-    if (-220 > ball.ycor() > -240) and (
-            (ball.xcor() < board.xcor() + 20) and (ball.xcor() > board.xcor() - 20)):
-        ball.sety(-220)
-        dy *= -1
+        elif ball.xcor() < -240:
+            ball.setx(-240)
+            dx *= -1
 
-    if b.isvisible() and (b.ycor() + 10 > ball.ycor() >= b.ycor()) and (
-            (ball.xcor() < b.xcor() + 20) and (ball.xcor() > b.xcor() - 20)):
-        b.ht()
-        dy *= -1
-
-# window.mainloop()
+        if (-220 > ball.ycor() > -240) and (
+                (ball.xcor() < board.xcor() + 20) and (ball.xcor() > board.xcor() - 20)):
+            ball.sety(-220)
+            dy *= -1
+            dx *= 1
+            score += 1
+            if score % 5 == 0:
+                multiplier += 100
+            else:
+                multiplier += 50
+            pen.goto((100, 200))
+            pen.clear()
+            pen.pd()
+            pen.write(f"Score : {score}", font=("Arial", 16, "normal"))
+            pen.pu()
+except Exception as e:
+    pass
+sys.exit()
